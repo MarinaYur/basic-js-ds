20884f752d6bb31c858853ee0acf58f0e478615f
MarinaYur <marina.yurkev@gmail.com>
2023-12-16 17:44:25 +0300

    feat: add solution stack task

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
+  constructor() {
+    this.stack = [];
+  }
   push(element) {
     // throw new NotImplementedError('Not implemented');
-    return this.push(element);
+    return this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    return this.stack.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // throw new NotImplementedError('Not implemented');
+    return this.stack.at(-1);
   }
 }
-
 module.exports = {
   Stack
 };
